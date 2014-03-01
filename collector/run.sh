11b88595ef2f92db7013965a858631b06fe2e135
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2014-03-01 23:46:42 +0400

    Added launch scripts

---
+++ collector/run.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+if test "$1" != debug -a "$1" != release
+then
+  echo "Usage $0 debug|release"
+  exit 1
+fi
+
+PROJECT=$(xmllint --xpath 'string(/project/@name)' build.xml) 
+if test -z $PROJECT
+then
+  echo Error: cannot parse ant build file build.xml >&2
+  exit 1
+fi
+echo Project: $PROJECT
+
+PACKAGE=$(xmllint --xpath 'string(/manifest/@package)' AndroidManifest.xml) 
+if test -z $PACKAGE 
+then
+  echo Error: cannot parse package manifest file AndroidManifest.xml >&2
+  exit 1
+fi
+echo Package: $PACKAGE
+
+if test `adb get-state| tail --lines=1` != device
+then
+  echo Error: device is not connected to the Android Debug Bridge
+fi
+
+NDK_PATH=/opt/android.com/android-ndk-r8b
+
+#Temporary workaround
+if [[ -d $NDK_PATH ]] 
+then
+  $NDK_PATH/ndk-build clean || exit 1
+  if test "$1" = debug
+  then
+    $NDK_PATH/ndk-build NDK_DEBUG=1 || exit 1
+  else
+    $NDK_PATH/ndk-build || exit 1
+  fi
+fi
+
+ant debug || exit 1
+#temp ant $1 || exit 1
+echo -n 'Uninstalling existing package version: '; adb uninstall $PACKAGE
+echo 'Installing package: '
+#temp adb -d install -r bin/$PROJECT-$1.apk
+adb -d install -r bin/$PROJECT-debug.apk
+
+if test "$1" = debug
+then
+  DEBUG_FLAGS='-D'
+else
+  DEBUG_FLAGS=''
+fi
+
+#The script launches first activity declared in the manifest file
+ACTIVITY=$(xmllint --xpath "string(/manifest/application/activity[1]/@*[namespace-uri()='http://schemas.android.com/apk/res/android' and local-name()='name'])" AndroidManifest.xml) 
+if [[ -n $ACTIVITY ]]
+then
+  echo 'Starting package activity: '$ACTIVITY
+  adb -d shell "am start $DEBUG_FLAGS -W -n $PACKAGE/$PACKAGE.$ACTIVITY"
+else
+  INTENT=$(xmllint --xpath "string(/manifest/application/service/intent-filter/action/@*[namespace-uri()='http://schemas.android.com/apk/res/android' and local-name()='name'])" AndroidManifest.xml)
+  if [[ -n $INTENT ]]
+  then
+    echo 'Starting package intent: '$INTENT
+    adb -d shell "am startservice $DEBUG_FLAGS -W $PACKAGE/$INTENT"
+  else
+    echo Error: cannot parse package manifest file AndroidManifest.xml >&2
+    exit 1
+  fi
+fi
+
+if test "$1" = debug
+then
+  while true
+  do
+    for pid in `adb -d jdwp`
+    do
+      name=$(adb shell ps $pid | awk -v RS=\\r\\n 'NR==2 {printf $NF}')
+      echo Found a process hosting a JDWP transport: PID=$pid, NAME = $name
+      if test $name = $PACKAGE
+      then
+        adb forward tcp:29882 jdwp:$pid
+        exec jdb -sourcepath ../funf-open-sensing-framework/src/ -connect com.sun.jdi.SocketAttach:hostname=localhost,port=29882
+      fi
+    done
+    echo Error: debug launch failure, will try again
+    sleep 1
+  done
+fi
